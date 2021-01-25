d200cc716ecaf3f99b6b8872fa2e5b2c0d070db5
MohammedZaid12 <zaid1805a@aptechgdn.net>
2021-01-25 22:42:33 +0500

    login service modifieed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,86 +7,86 @@ import { Auth } from './auth.entit';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 
-
-
 @Injectable()
 export class AuthService {
-    constructor(@InjectRepository(User) private authRepository: Repository<User>, private jwtService: JwtService) { }
+  constructor(
+    @InjectRepository(User) private authRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
 
-    async login(user: Auth) {
-        const { email, password } = user;
-        const data = await this.authRepository.findOne({ where: { email } });
-        if (!data) {
-            let errorResponse = new HttpException('User not found', HttpStatus.UNAUTHORIZED);
-            return {
-                errorMessage: errorResponse.getResponse(),
-                status: errorResponse.getStatus()
-            };
-
-        }
-
-        const areEqual = await bcrypt.compare(user.password, data.password);
-        if (!areEqual) {
-            let errorResponse = new HttpException('Invalid credentials given', HttpStatus.UNAUTHORIZED);
-            return {
-                errorMessage: errorResponse.getResponse(),
-                status: errorResponse.getStatus()
-            };
-        }
-        const returnResponse = {
-            ...this.token(data),
-            Id: data.id,
-            name: data.name,
-            email: data.email
-        };
-        return {
-            message: "User logged In",
-            user: returnResponse
-        }
-
+  async login(user: Auth) {
+    const { email, password } = user;
+    const data = await this.authRepository.findOne({ where: { email } });
+    if (!data) {
+      let errorResponse = new HttpException(
+        'User not found',
+        HttpStatus.UNAUTHORIZED,
+      );
+      return {
+        message: errorResponse.getResponse(),
+        status: errorResponse.getStatus(),
+      };
     }
 
-    async register(data: User) {
-
-        const check = await this.authRepository.findOne({ where: [{ "email": data.email }] });
-        if (check) {
-            let errorResponse = new HttpException('User already exists', HttpStatus.UNAUTHORIZED);
-            return {
-                errorMessage: errorResponse.getResponse(),
-                status: errorResponse.getStatus()
-            };
-
-        }
-        else {
-
-            const hashedPassword = await bcrypt.hash(data.password, 10);
-            const user = this.authRepository.create({ ...data, password: hashedPassword});
-            this.authRepository.insert(user);
-        
-            return {
-                message:'User added Successfully'
-            };
-        }
-
-
-
+    const areEqual = await bcrypt.compare(user.password, data.password);
+    if (!areEqual) {
+      let errorResponse = new HttpException(
+        'Invalid credentials given',
+        HttpStatus.UNAUTHORIZED,
+      );
+      return {
+        message: errorResponse.getResponse(),
+        status: errorResponse.getStatus(),
+      };
     }
 
-    private token({ email }: Auth) {
-        let user: JwtPayload = { email };
-        let accessToken = this.jwtService.sign(user);
-        return {
-            expiresIn: '7d',
-            accessToken,
-        };
+    return {
+      message: 'User logged In',
+      Id: data.id,
+      name: data.name,
+      email: data.email,
+      token:this.token(data),
+      status: HttpStatus.OK  
+       };
+  }
+
+  async register(data: User) {
+    const check = await this.authRepository.findOne({
+      where: [{ email: data.email }],
+    });
+    if (check) {
+      let errorResponse = new HttpException(
+        'User already exists',
+        HttpStatus.UNAUTHORIZED,
+      );
+      return {
+        message: errorResponse.getResponse(),
+        status: errorResponse.getStatus(),
+      };
+    } else {
+      const hashedPassword = await bcrypt.hash(data.password, 10);
+      const user = this.authRepository.create({
+        ...data,
+        password: hashedPassword,
+      });
+      this.authRepository.insert(user);
+
+      return {
+        message: 'User added Successfully',
+      };
     }
+  }
 
-    async findByPayload({ email }: any): Promise<User> {
-        return await this.authRepository.findOne({
-            where: { email }
-        });
-    }
+  private token({ email }: Auth) {
+    let user: JwtPayload = { email };
+    let accessToken = this.jwtService.sign(user);
+    return accessToken;
+    
+  }
 
-
-
+  async findByPayload({ email }: any): Promise<User> {
+    return await this.authRepository.findOne({
+      where: { email },
+    });
+  }
 }
